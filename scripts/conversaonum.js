ae4965379fce96b13b8588d1d482d73e606567a7
AquaMouzinho <42475699+AquaMouzinho@users.noreply.github.com>
2022-10-27 13:16:25 -0300

    :sparkles: feat: Conversão de Sistemas
    Adição da funcionalidade de Conversão de Números para outros sistemas numéricos à Calculadora (Em construção)

---
+++ scripts/conversaonum.js
@@ -0,0 +1,21 @@
+function conversao(numero, base_inicial, base_final) {
+  let resultado = '';
+  if (base_inicial == base_final) {
+    resultado = numero.toString();
+  } else {
+    switch (base_inicial) {
+      case 'binario':
+
+        break;
+      case 'decimal':
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return resultado;
+}
+
+var btnConversao = document.getElementById('btnConvSis');
